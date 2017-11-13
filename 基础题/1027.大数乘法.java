9e1b92601ba47db586ace08d01fb18307992600d
ln0411 <475704531@qq.com>
2017-11-13 20:22:43 +0800

    1027 java

---
+++ 基础题/1027.大数乘法.java
@@ -0,0 +1,27 @@
+/*
+1027 大数乘法
+基准时间限制：1 秒 空间限制：131072 KB 分值: 0 难度：基础题 收藏  关注
+给出2个大整数A,B，计算A*B的结果。
+Input
+第1行：大数A
+第2行：大数B
+(A,B的长度 <= 1000，A,B >= 0）
+Output
+输出A * B
+Input示例
+123456
+234567
+Output示例
+28958703552
+ */
+import java.util.*;
+import java.math.*;
+public class main{ 
+    public static void main(String[] args) { 
+        Scanner input = new Scanner(System.in);
+        BigInteger a,b;
+        a = input.nextBigInteger();
+        b = input.nextBigInteger();
+        System.out.println(a.multiply(b));
+    }
+}
